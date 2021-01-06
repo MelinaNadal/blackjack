2a761008d37e97d5888df906343796b3d418b2af
Melina Nadal <melinanhopkins@gmail.com>
2021-01-05 20:45:51 -0500

    final commit

--- blackjack.rb
+++ blackjack.rb
@@ -1,4 +1,42 @@
-require 'Colorize'
+class Player 
+	attr_accessor :name, :hand, :bankroll, :total
+	def initialize name, hand, bankroll, total
+		@name = name
+		@hand = []
+		@bankroll = bankroll
+		@total = total
+	end
+end
+
+player = Player.new "Player", [], 100, 0
+the_house = Player.new "The House", [], 10000, 0	
+
+
+class Deck
+	def initialize
+		@faces = [*(2..10), 'Jack', 'Queen', 'King', 'Ace']
+		@suits = ['♣︎', '♠︎', '❤️', '♦︎']
+		@cards = []
+		@faces.each do |face|
+			if face.class == Integer
+				value = face
+			elsif face == 'Ace'
+				value = 11
+			else 
+				value = 10
+			end
+			@suits.each do |suit|
+				@cards << Card.new(face, suit, value)
+			end 
+		end
+	@cards.shuffle!
+	end 				
+	def deal_cards(num, player)
+		num.times{@cards.shift.generate_card(player)}
+	end 
+end 		
+
+
 class Card
 
 	attr_reader :face, :suit, :value
@@ -8,82 +46,21 @@ class Card
 		@value = value
 	end
 
-	def value
-		return 10 if ["J", "Q", "K"].include?(@value)
-		return 11 if @value =="A"
-		return @value
+	def generate_card (player)
+		new_card=Card.new face, suit, value
+		player.hand << new_card
+		player.total += new_card.value
 	end
-
-	def to_s
-		"#{@value}-#{suit}"
 	end
-end	
-
-class Deck
-	attr_reader :cards
-
-	def initialize
-		@cards = Deck.build_cards
-	end
-	def deal_card player
-        new_card = Card.new face, suit, value
-        player.hand << new_card
-        player.total = player.total + new_card.value
-    end 
-end
+	
 
 
-	def self.build_cards
-		cards = []
-		[:clubs, :diamonds, :spades, :hearts].each do |suit|
-			(2..10).each do |number|
-				cards << Card.new(suit, number)
-			end
-			["J", "Q", "K", "A"].each do |facecard|
-				cards << Card.new(suit, facecard)
-			end
-			class Player 
-				attr_accessor :name, :hand, :bankroll, :total
-				def initialize name, hand, bankroll, total
-					@name = name
-					@hand = []
-					@bankroll = bankroll
-					@total = total
-				end
-			end
-			player = Player.new "Player", [], 100, 0
-			the_house = Player.new "The House", [], 10000, 0	
-			class Deck
-				def initialize
-					@faces = [*(2..10), 'Jack', 'Queen', 'King', 'Ace']
-					@suits = ['♣︎', '♠︎', '❤️', '♦︎']
-					@cards = []
-					@faces.each do |face|
-						if face.class == Integer
-							value = face
-						elsif face == 'Ace'
-							value = 11
-						else 
-							value = 10
-						end
-						@suits.each do |suit|
-							@cards << Card.new(face, suit, value)
-						end 
-					end
-				@cards.shuffle!
-				end 				
-				def deal_cards(num, player)
-					num.times {@cards.shift.deal_card(player)}
-				end 
-			end 		
-			def welcome(name)
-				print(name)
-				gets
-			end 	
-			name = welcome("This is  to Ruby Blackjack! What's your name? ").chomp
-puts "\nAre you Ready to gamble #{name.green}!?"
 
-player.name="#{name.green}"
+		puts "This is  to Ruby Blackjack! What's your name? "
+		name = gets.chomp
+puts "\nAre you Ready to gamble #{name}!?"
+
+player.name="#{name}"
 
 loop do
     puts "(D)eal or (Q)uit"
@@ -98,13 +75,13 @@ loop do
         the_house.hand.clear
         the_house.total = 0
 
-		puts "#{player.name.green}, you were dealt:"
+		puts "#{player.name}, you were dealt:"
 		deck = Deck.new
         deck.deal_cards(2, player)
         puts "The #{player.hand[0].face} of #{player.hand[0].suit} (#{player.hand[0].value})"
 		puts "The #{player.hand[1].face} of #{player.hand[1].suit} (#{player.hand[1].value})"
 		deck.deal_cards(2, the_house)
-        puts "\n#{the_house.name.red}"
+        puts "\n#{the_house.name}"
         puts "The #{the_house.hand[0].face} of #{the_house.hand[0].suit} (#{the_house.hand[0].value})"
 
         until player.total >= 21 do
@@ -112,7 +89,7 @@ loop do
             hit = gets.chomp.downcase
                 if hit == 'h'
                     deck.deal_cards(1, player)
-                    puts "\n#{player.name.green}, your new card is:"
+                    puts "\n#{player.name}, your new card is:"
                     puts "The #{player.hand[0].face} of #{player.hand[0].suit} (#{player.hand[0].value})\n"
                     puts "The total in your hand is: #{player.total}"
 				    puts "\n\nThe dealer has one card showing: The #{the_house.hand[0].face} of #{the_house.hand[0].suit} (#{the_house.hand[0].value})"
@@ -120,34 +97,34 @@ loop do
                     break
                 end
             end
-			puts "\n\n#{player.name.green}, your hand has a total value of #{player.total}"
-            puts "The #{the_house.name.red} has a hand value of #{the_house.total}\n"
+			puts "\n\n#{player.name}, your hand has a total value of #{player.total}"
+            puts "The #{the_house.name} has a hand value of #{the_house.total}\n"
 			if player.total <= 21 && the_house.total <=21
 				if player.total > the_house.total
-					puts "\n#{player.name.green}, you won!"
+					puts "\n#{player.name}, you won!"
 					player.bankroll += 10
 					the_house.bankroll -= 10
 				elsif player.total < the_house.total
-					puts "\nThe #{the_house.name.red} won!"
+					puts "\nThe #{the_house.name} won!"
 					player.bankroll -= 10
 					the_house.bankroll += 10
 				else 
 					puts "\nIts a tie"
 				end 
 			elsif player.total > 21 && the_house.total <= 21
-				puts "\n\n#{player.name.green}, you have gone over 21 and lose this round."
+				puts "\n\n#{player.name}, you have gone over 21 and lose this round."
 				player.bankroll -= 10
 				the_house.bankroll += 10
 					
 			elsif player.total <= 21 && the_house.total > 21
-				puts "#{the_house.name.red} went over 21. #{player.name.green}, you win this round."
+				puts "#{the_house.name} went over 21. #{player.name}, you win this round."
 				player.bankroll -= 10
 				the_house.bankroll += 10
 			else player.total > 21 && the_house.total > 21
-				puts "#{player.name.green}, you and #{the_house.name.red} have tied"
+				puts "#{player.name}, you and #{the_house.name} have tied"
 			end 
-			puts "\n\n#{player.name.green}, you now have $#{player.bankroll}."
-			puts "#{the_house.name.red}, now has $#{the_house.bankroll}."
+			puts "\n\n#{player.name}, you now have $#{player.bankroll}."
+			puts "#{the_house.name}, now has $#{the_house.bankroll}."
 			if player.bankroll <= 0
 				puts "\nYou're new balance is $0 YOU LOSE"
 				break
